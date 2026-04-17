5ef72258a97ec02e56c5014fb775076f31f19a59
Orel Tzur <56762076+FloorRule@users.noreply.github.com>
2026-04-17 23:21:04 +0300

    OpenGL Hello World

---
+++ VOID-STACK.cpp
@@ -0,0 +1,125 @@
+﻿// VOID-STACK.cpp : Defines the entry point for the application.
+//
+
+#include "VOID-STACK.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+#include "Shader.h"
+
+// Function prototypes (We keep main clean)
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main() {
+    // 1. Initialize GLFW
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+    
+    // 2. Configure GLFW (Setting OpenGL to version 3.3, Core Profile)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // 3. Create the Window
+    GLFWwindow* window = glfwCreateWindow(1200, 800, "VOID-STACK ENGINE", NULL, NULL);
+    if (window == NULL) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // 4. Initialize GLAD (Load all OpenGL function pointers)
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // 5. Tell OpenGL the size of the rendering window
+    glViewport(0, 0, 1200, 800);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+    // 6. Shaders
+    Shader ourShader("D:\\project2\\VOID-STACK\\shaders\\shaderVS.txt", "D:\\project2\\VOID-STACK\\shaders\\shaderFS.txt");
+    ourShader.use();
+    // 7. Geometry
+
+    float vertices[] = {
+        // position     |    // color
+        0.5f,-0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom right
+        -0.5f,-0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left
+        0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f // top
+    };
+
+    unsigned int VAO;
+    unsigned int VBO;
+    
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    // 1. bind Vertex Array Object
+    glBindVertexArray(VAO);
+
+    // 2. copy our vertices array in a buffer for OpenGL to use
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // 3. then set our vertex attributes pointers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // 8. The RENDER LOOP (The Heartbeat)
+    while (!glfwWindowShouldClose(window)) {
+        // Input
+        processInput(window);
+
+        // Rendering Commands
+        // Deep Space Navy Color (0.05, 0.05, 0.1)
+        glClearColor(0.05f, 0.05f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ourShader.use();
+
+        glm::mat4 trans = glm::mat4(1.0f);
+
+        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+
+        trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
+
+        ourShader.setMat4("transform", trans);
+
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // Swap buffers and poll IO events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // 9. Cleanup
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glfwTerminate();
+    return 0;
+}
+
+// Handle window resizing
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+// Handle key presses
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
